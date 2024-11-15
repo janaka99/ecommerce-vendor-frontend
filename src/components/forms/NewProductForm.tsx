762345201a5d75b0b91d7f5a34ba7b70ba0981b6
janaka99 <janakachamith88@gmail.com>
2024-11-15 14:42:55 +0530

    Edit page update

--- src/components/forms/NewProductForm.tsx
+++ src/components/forms/NewProductForm.tsx
@@ -170,172 +170,170 @@ export default function NewProductForm() {
   };
 
   return (
-    <Container>
-      <form onSubmit={newProductForm.handleSubmit(onSubmit)}>
-        <div className="flex w-full  flex-col  lg:flex-row lg:gap-10">
-          <FormField
-            control={newProductForm.control}
-            name="sku"
-            render={({ field, fieldState }) => (
-              <FormItem>
-                <FormCol>
-                  <FormLabel text="SKU" />
-                  <FormInput {...field} type="text" />
-                </FormCol>
-                <FormMessage {...fieldState} />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={newProductForm.control}
-            name="price"
-            render={({ field, fieldState }) => (
-              <FormItem>
-                <FormCol>
-                  <FormLabel text="Price" />
-                  <FormInput
-                    {...field}
-                    type="number"
-                    onChange={(e) => {
-                      const value = parseFloat(e.target.value);
-                      field.onChange(isNaN(value) ? 0 : value);
-                    }}
-                  />
-                </FormCol>
-                <FormMessage {...fieldState} />
-              </FormItem>
-            )}
-          />
-        </div>
-        <div className="flex w-full  flex-col  lg:flex-row lg:gap-10">
-          <FormField
-            control={newProductForm.control}
-            name="name"
-            render={({ field, fieldState }) => (
-              <FormItem>
-                <FormCol>
-                  <FormLabel text="Name" />
-                  <FormInput {...field} type="text" />
-                </FormCol>
-                <FormMessage {...fieldState} />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={newProductForm.control}
-            name="qty"
-            render={({ field, fieldState }) => (
-              <FormItem>
-                <FormCol>
-                  <FormLabel text="QTY" />
-                  <FormInput
-                    {...field}
-                    type="number"
-                    onChange={(e) => {
-                      const value = parseFloat(e.target.value);
-                      field.onChange(isNaN(value) ? 0 : value);
-                    }}
-                  />
-                </FormCol>
-                <FormMessage {...fieldState} />
-              </FormItem>
-            )}
-          />
-        </div>
+    <form onSubmit={newProductForm.handleSubmit(onSubmit)}>
+      <div className="flex w-full  flex-col  lg:flex-row lg:gap-10">
         <FormField
           control={newProductForm.control}
-          name="description"
+          name="sku"
           render={({ field, fieldState }) => (
             <FormItem>
-              <FormLabel text="Description" />
-              <p className="text-secondary-text text-primary">
-                A small description about the project"
-              </p>
-              <FormTextArea {...field} />
+              <FormCol>
+                <FormLabel text="SKU" />
+                <FormInput {...field} type="text" />
+              </FormCol>
               <FormMessage {...fieldState} />
             </FormItem>
           )}
         />
-
         <FormField
           control={newProductForm.control}
-          name="images"
+          name="price"
           render={({ field, fieldState }) => (
             <FormItem>
-              <div className="flex gap-10">
-                <div className="">
-                  <FormCol>
-                    <FormLabel text="Product Images" />
-                  </FormCol>
-                  <p className="text-secondary-text text-primary max-w-40">
-                    JPEG, PNG, SVG or GIF (Maximum file size 50MB)
-                  </p>
-                </div>
-                {imagePreviews.length > 0 && (
-                  <div className="mt-3 flex gap-2">
-                    {imagePreviews.map((prw, index) => (
-                      <div
-                        key={index}
-                        className="relative w-[85px] aspect-square"
-                      >
-                        <img
-                          key={index}
-                          src={prw.src}
-                          alt={`Preview ${index + 1}`}
-                          className="w-[85px] aspect-square object-cover rounded"
-                        />
-                        <button
-                          type="button"
-                          onClick={() => removeImageFromForm(index)}
-                          className="absolute top-2 right-2 text-red-500 bg-background cursor-pointer hover:bg-secondary"
-                        >
-                          <BiX />
-                        </button>
-                        <button
-                          type="button"
-                          onClick={() => selectMainImage(index)}
-                          className={`absolute bottom-2 left-2 text-white px-2 py-1 text-xs ${
-                            mainImageIndex === index
-                              ? "bg-green-600"
-                              : "bg-gray-400"
-                          } rounded`}
-                        >
-                          {mainImageIndex === index ? "Main" : "Set as Main"}
-                        </button>
-                      </div>
-                    ))}
-                  </div>
-                )}
-                <input
-                  type="file"
-                  id="file"
-                  className="hidden"
-                  multiple
-                  accept="image/*"
-                  onChange={handleImageChange}
+              <FormCol>
+                <FormLabel text="Price" />
+                <FormInput
+                  {...field}
+                  type="number"
+                  onChange={(e) => {
+                    const value = parseFloat(e.target.value);
+                    field.onChange(isNaN(value) ? 0 : value);
+                  }}
                 />
-                <div className="flex gap-5 items-start mb-5">
-                  <label
-                    htmlFor="file"
-                    className=" text-primary-text text-accent underline font-bold cursor-pointer"
-                  >
-                    Add Images
-                  </label>
-                </div>
-              </div>
-
-              <span className="text-xs mt-[2px] text-red-400 font-semibold flex justify-start">
-                {fieldState.error?.message}
-              </span>
+              </FormCol>
+              <FormMessage {...fieldState} />
             </FormItem>
           )}
         />
-        <FormError message={error} />
-        <FormSuccess message={success} />
-        <div className="flex justify-end">
-          <FormButton text="Publish" isProcessing={isLoading} />
-        </div>
-      </form>
-    </Container>
+      </div>
+      <div className="flex w-full  flex-col  lg:flex-row lg:gap-10">
+        <FormField
+          control={newProductForm.control}
+          name="name"
+          render={({ field, fieldState }) => (
+            <FormItem>
+              <FormCol>
+                <FormLabel text="Name" />
+                <FormInput {...field} type="text" />
+              </FormCol>
+              <FormMessage {...fieldState} />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={newProductForm.control}
+          name="qty"
+          render={({ field, fieldState }) => (
+            <FormItem>
+              <FormCol>
+                <FormLabel text="QTY" />
+                <FormInput
+                  {...field}
+                  type="number"
+                  onChange={(e) => {
+                    const value = parseFloat(e.target.value);
+                    field.onChange(isNaN(value) ? 0 : value);
+                  }}
+                />
+              </FormCol>
+              <FormMessage {...fieldState} />
+            </FormItem>
+          )}
+        />
+      </div>
+      <FormField
+        control={newProductForm.control}
+        name="description"
+        render={({ field, fieldState }) => (
+          <FormItem>
+            <FormLabel text="Description" />
+            <p className="text-secondary-text text-primary">
+              A small description about the project"
+            </p>
+            <FormTextArea {...field} />
+            <FormMessage {...fieldState} />
+          </FormItem>
+        )}
+      />
+
+      <FormField
+        control={newProductForm.control}
+        name="images"
+        render={({ field, fieldState }) => (
+          <FormItem>
+            <div className="flex gap-10">
+              <div className="">
+                <FormCol>
+                  <FormLabel text="Product Images" />
+                </FormCol>
+                <p className="text-secondary-text text-primary max-w-40">
+                  JPEG, PNG, SVG or GIF (Maximum file size 50MB)
+                </p>
+              </div>
+              {imagePreviews.length > 0 && (
+                <div className="mt-3 flex gap-2">
+                  {imagePreviews.map((prw, index) => (
+                    <div
+                      key={index}
+                      className="relative w-[85px] aspect-square"
+                    >
+                      <img
+                        key={index}
+                        src={prw.src}
+                        alt={`Preview ${index + 1}`}
+                        className="w-[85px] aspect-square object-cover rounded"
+                      />
+                      <button
+                        type="button"
+                        onClick={() => removeImageFromForm(index)}
+                        className="absolute top-2 right-2 text-red-500 bg-background cursor-pointer hover:bg-secondary"
+                      >
+                        <BiX />
+                      </button>
+                      <button
+                        type="button"
+                        onClick={() => selectMainImage(index)}
+                        className={`absolute bottom-2 left-2 text-white px-2 py-1 text-xs ${
+                          mainImageIndex === index
+                            ? "bg-green-600"
+                            : "bg-gray-400"
+                        } rounded`}
+                      >
+                        {mainImageIndex === index ? "Main" : "Set as Main"}
+                      </button>
+                    </div>
+                  ))}
+                </div>
+              )}
+              <input
+                type="file"
+                id="file"
+                className="hidden"
+                multiple
+                accept="image/*"
+                onChange={handleImageChange}
+              />
+              <div className="flex gap-5 items-start mb-5">
+                <label
+                  htmlFor="file"
+                  className=" text-primary-text text-accent underline font-bold cursor-pointer"
+                >
+                  Add Images
+                </label>
+              </div>
+            </div>
+
+            <span className="text-xs mt-[2px] text-red-400 font-semibold flex justify-start">
+              {fieldState.error?.message}
+            </span>
+          </FormItem>
+        )}
+      />
+      <FormError message={error} />
+      <FormSuccess message={success} />
+      <div className="flex justify-end">
+        <FormButton text="Publish" isProcessing={isLoading} />
+      </div>
+    </form>
   );
 }
--- src/pages/NewProductPage.tsx
+++ src/pages/NewProductPage.tsx
@@ -1,11 +1,13 @@
 import Container from "../components/layouts/Container";
 import NewProductForm from "../components/forms/NewProductForm";
+import FormHeader from "../components/ui/FormHeader";
 
 type Props = {};
 
 export default function NewProductPage({}: Props) {
   return (
-    <Container className="py-10">
+    <Container className="py-10 flex flex-col gap-10 ">
+      <FormHeader text="Edit product" />
       <NewProductForm />
     </Container>
   );
